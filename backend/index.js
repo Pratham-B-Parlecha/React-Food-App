35804ccd44a5e5f0c7e1024931208d1846fd8177
PRATHAMPARLECHA <prathamparlecha09@gmail.com>
2024-04-21 13:18:13 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs/promises';
+import cors from "cors";
 
 import bodyParser from 'body-parser';
 import express from 'express';
@@ -8,6 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+app.use(cors({ origin: 'https://react-food-app-gray.vercel.app' }))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
